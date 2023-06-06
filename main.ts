513f91a6d800a0a9d1e2136a6c9cc954e5feae41
GlubuxTV <121926553+GlubuxTV@users.noreply.github.com>
2023-06-06 16:59:50 +0200

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -8,25 +8,45 @@ function attack_1 (nr: number) {
         basic.pause(200)
         maqueen.motorStop(maqueen.Motors.M1)
         basic.pause(500)
-        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, 100)
-        basic.pause(300)
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, 255)
+        basic.pause(200)
         maqueen.motorStop(maqueen.Motors.All)
         maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOff)
         maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
     }
 }
+input.onButtonPressed(Button.A, function () {
+    fehrnsicht = 0
+    while (fehrnsicht == 0) {
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 30)
+        if (maqueen.Ultrasonic(PingUnit.Centimeters) <= 20) {
+            fehrnsicht = 1
+            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 30)
+            maqueen.motorStop(maqueen.Motors.All)
+        }
+    }
+    maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 255)
+})
+// Tobias, deine Attack
+function Attack_2 (num: number) {
+	
+}
 let attack1 = 0
 let status = 0
-maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 255)
+let fehrnsicht = 0
+let strip = neopixel.create(DigitalPin.P15, 24, NeoPixelMode.RGB)
+strip.showColor(neopixel.colors(NeoPixelColors.Indigo))
 basic.forever(function () {
     if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1 || maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 1) {
         maqueen.motorStop(maqueen.Motors.All)
+        strip.showColor(neopixel.colors(NeoPixelColors.Red))
         status = 505
         maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, 255)
         basic.pause(500)
         maqueen.motorStop(maqueen.Motors.M1)
         basic.pause(1200)
         maqueen.motorStop(maqueen.Motors.All)
+        strip.showColor(neopixel.colors(NeoPixelColors.Indigo))
     } else if (maqueen.Ultrasonic(PingUnit.Centimeters) <= 20) {
         attack1 = 0
     }
